486c355c4a8669c7be32e7ee9e5413f4e3859732
Amandine <michel@ubuntu.(none)>
2011-03-05 14:48:27 +0100

    Deuxième test : récupération d'un fichier txt

---
+++ TestMatR2.java
@@ -0,0 +1,14 @@
+public class TestMatR2{
+
+    public static void main (String [] args){
+	try{
+    MatR mat1 = new MatR("data1.txt");
+    mat1.afficheSimple("Data");
+    System.out.println(mat1.get(1,1));
+    System.out.println(mat1.get(1,2));
+    System.out.println(mat1.get(2,1));
+	}
+	catch(Exception e){};
+    }
+
+}
