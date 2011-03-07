dde1d9792a245b941efe8fb6f8344c599f979266
David <david.delanoue@etu.univ-lyon1.fr>
2011-03-07 14:17:45 +0100

    et première version (à tester)

--- Acp.java
+++ Acp.java
@@ -2,6 +2,7 @@ import java.* ;
 // import java MatR aussi!!!
 
 class Acp {
+
     MatR donnee ; // matrice des données
 
     boolean reduit ; // si la matrice doit être réduite ou pas
@@ -12,25 +13,32 @@ class Acp {
 
     MatR vecteurPropre ; // Matrice des vecteurs propres
 
-    double [] ponderation ; // pondération des individus
+    MatR ponderation ; // pondération des individus
+
+    MatR composantes ;
 
     public Acp() {} 
 
-    public Acp(MatR m,boolean b1,boolean b2) {
+    public Acp(MatR m,boolean b1,boolean b2,MatR pd) {
 	donnee = m;
 	reduit = b1;
 	centre = b2;
+	ponderation = pd;
 
 	// centrage et réduction des données si spécifié
 	if (centre)
-	    donnee = MatR.centrer();
+	    donnee = donnee.centrer(ponderation);
 	if (reduit)
-	    donnee = MatR.reduire();
+	    donnee = donne.reduire();
 	// calcul des valeurs propres et vecteurs propres
 	propre(vecteurPropre,valeurPropre);
 
 	correlation = donnee.XtX();
 	
+	composantes = donnee.getProjectionOrth(correlation);
+	
+
+
     }
 
     public void correlation() {
