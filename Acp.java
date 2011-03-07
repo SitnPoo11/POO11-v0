1cf3d88bb9739c2786d48103eb83908a672df452
David <david.delanoue@etu.univ-lyon1.fr>
2011-03-07 13:50:24 +0100

    debut de la class Acp

---
+++ Acp.java
@@ -0,0 +1,42 @@
+import java.* ;
+// import java MatR aussi!!!
+
+class Acp {
+    boolean donnee ; // matrice des données
+
+    boolean reduit ; // si la matrice doit être réduite ou pas
+    boolean centre ; // si la matrice doit être centree ou pas
+
+    MatR correlation ; // matrice des corrélations 
+    MatR [] valeurPropre ; // vecteur des valeurs propres
+
+    MatR vecteurPropre ; // Matrice des vecteurs propres
+
+    double [] ponderation ; // pondération des individus
+
+    public Acp() {} 
+
+    public Acp(MatR m,boolean b1,boolean b2) {
+	donnee = m;
+	reduit = b1;
+	centre = b2;
+
+	// centrage et réduction des données si spécifié
+	if (centre)
+	    donnee = MatR.centrer();
+	if (reduit)
+	    donnee = MatR.reduire();
+	// calcul des valeurs propres et vecteurs propres
+	propre(vecteurPropre,valeurPropre);
+
+	MatR temp = donnee.XtX();
+	
+    }
+
+    public void correlation() {
+	
+    }
+
+    public void propre() {
+	MatR.diag(vecteurPropre,valeurPropre);
+    }
