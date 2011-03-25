b7f10e49fe5cb5cafe3215d356e4ab027f587911
Amandine <berger@euclid.univ-lyon1.fr>
2011-03-25 09:50:27 +0100

    De nouveaux tests

--- tests/TestMatR.java
+++ tests/TestMatR.java
@@ -14,27 +14,23 @@ public class TestMatR extends TestCase{
 	super(name);
     }
 
-    // public void testTranspose(){
-    // 	//    	MatR mat1 = new MatR(2,2,1);
-    // 	//MatR mat2 = new MatR(2,2,1);
 
-    // 	double[][] val1 = {{1,2,3},{2,1,3},{5,4,3}};
-    // 	//MatR mat1 = new MatR(val1);
-    // 	double[][] val2 = {{1,4,9},{4,1,9},{25,16,9}};
-    // 	MatR mat2 = new MatR(val2);
+    public void testTranspose(){
+	double[][] f1 = {{1,2,3},{4,5,6},{7,8,9}};  // génération d'un MatR à partir d'un tableau de double
+    	MatR mat1 = new MatR(f1);
+    	MatR mat2 = mat1.tr();  //Transposition
+    	double[][] f2 = mat2.MatRToDouble();  //Passage de MatR à tableau de double
+    	MatR mat3 = new MatR(f2);
+    	MatR mat4 = mat3.extraireBloc(2,mat3.n(),2,mat3.p());  // Extraction d'un bloc
 
-    // 	Fonction f1 = new Fonction(val1);
-    // 	Fonction f2 = new Fonction(val2);
+	double[][] f3 = {{1,4,7},{2,5,8},{3,6,9}};
+    	Fonction f4 = new Fonction(f3);
 
-    // 	double[] tab3 = f1.get(f1);
-    // 	double[] tab4 = f2.get(f2);
-	
-    // 	assertArrayEquals(tab3,tab4,0.1);
+ 	double[] tab1 = Fonction.get(mat4);
+ 	double[] tab2 = Fonction.get(f4);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
 
-    // 	//for (int i=1;i<=mat1.n();i++)
-    // 	//for (int j=1;j<=mat1.p();j++)
-    // 	//assertTrue(mat2.get(i,j) == mat1.transpose().get(j,i));
-    // }
 
     public void testMaximum(){
 	double[][] val1 = {{15,1,2},{5,1,1},{1,1,20}};
@@ -50,22 +46,23 @@ public class TestMatR extends TestCase{
 	assertArrayEquals(tab1,tab2,0.1);
     }
 
-    // public void testMinimum(){
-    // 	MatR mat3 = new MatR(3,3,1);
-    // 	mat3.set(1,1,15);
-    // 	mat3.set(1,3,2);
-    // 	mat3.set(2,1,5);
-    // 	mat3.set(3,3,20);
-    // 	MatR mat4 = new MatR(1,3,1);
+    public void testMinimum(){
+	MatR mat1 = new MatR(3,3,1);
+	mat1.set(1,1,15);  
+	mat1.set(1,3,-1);
+	mat1.set(2,1,5);
+	mat1.set(3,3,20);
+	MatR mat1bis = mat1.minColonnes();
 
-    // 	// double[][] tab3 = get(mat3);
-    // 	// double[][] tab4 = get(mat4);
-	
-    // 	// assertEquals(tab3,tab4);
+	double[][] val2 = {{1},{1},{-1}};
+    	Fonction f2 = new Fonction(val2);
 
-    // 	    //for (int i=1;i<=mat4.p();i++)
-    // 	    //assertTrue(mat4.get(1,i)==mat3.minColonnes().get(1,i));
-    // }
+ 	double[] tab1 = Fonction.get(mat1bis);
+ 	double[] tab2 = Fonction.get(f2);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
+
+ 
 
     // public void testMatrice(){
     // 	MatR mat1 = new MatR(2,2,1);
@@ -91,22 +88,115 @@ public class TestMatR extends TestCase{
 
     }
 
-    // public void testCarre(){
-    // 	double[][] val1 = {{1,2,3},{2,1,3},{5,4,3}};
-    // 	MatR mat1 = new MatR(val1);
-    // 	double[][] val2 = {{1,4,9},{4,1,9},{25,16,9}};
-    // 	MatR mat2 = new MatR(val2);
+    public void testVectToDiag(){
+	MatR mat1 = new MatR(1,3,1);
+	mat1.set(1,3,-1);
+	MatR mat11 = mat1.vectToDiag();
 
-    // 	// double[][] tab3 = get(mat1);
-    // 	// double[][] tab4 = get(mat2);
-	
-    // 	// assertEquals(tab3,tab4);
+	double[][] val2 = {{1,0,0},{0,1,0},{0,0,-1}};
+    	Fonction f2 = new Fonction(val2);
 
-    // 	//for (int i=1;i<=mat1.n();i++)
-    // 	//  for (int j=1;j<=mat1.n();j++)
-    // 	//	assertTrue(mat2.get(i,j) == mat1.carre().get(i,j));
+ 	double[] tab1 = Fonction.get(mat11);
+ 	double[] tab2 = Fonction.get(f2);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
 
-    // }
+    public void testSomme(){
+	double[][] f1 = {{1,2,3},{4,5,6},{7,8,9}};  
+    	MatR mat1 = new MatR(f1);
+	double[][] f2 = {{-1,3,5},{0,5,4.5},{3,-8,-11}}; 
+    	MatR mat2 = new MatR(f2);
+	mat1.addMat(mat2);
 
+	double[][] f3 = {{0,5,8},{4,10,10.5},{10,0,-2}};
+   	Fonction f4 = new Fonction(f3);
+
+ 	double[] tab1 = Fonction.get(mat1);
+ 	double[] tab2 = Fonction.get(f4);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
+
+    public void testProduitCste(){
+	double[][] f1 = {{1,2,3},{4,5,6}};  
+    	MatR mat1 = new MatR(f1);
+	mat1.multCste(2.5);
+
+	double[][] f3 = {{2.5,5,7.5},{10,12.5,15}};
+   	Fonction f4 = new Fonction(f3);
+
+ 	double[] tab1 = Fonction.get(mat1);
+ 	double[] tab2 = Fonction.get(f4);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
+
+   public void testDivisionCste(){
+	double[][] f3 = {{2.5,5,7.5},{10,12.5,15}};
+    	MatR mat1 = new MatR(f3);
+	mat1.divCste(2.5);
+
+	double[][] f1 = {{1,2,3},{4,5,6}};  
+   	Fonction f4 = new Fonction(f1);
+
+ 	double[] tab1 = Fonction.get(mat1);
+ 	double[] tab2 = Fonction.get(f4);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
+
+
+    public void testCarre(){
+    	double[][] val1 = {{1,2,3},{-2,1,-3},{5,4,3}};
+    	MatR mat1 = new MatR(val1);
+	mat1.carre();
+
+    	double[][] val2 = {{1,4,9},{4,1,9},{25,16,9}};
+   	Fonction f2 = new Fonction(val2);
+
+ 	double[] tab1 = Fonction.get(mat1);
+ 	double[] tab2 = Fonction.get(f2);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
+
+
+    public void testRacineCarree(){
+	double[][] val1 = {{1,4,9},{4,1,9},{25,16,9}};
+    	MatR mat1 = new MatR(val1);
+	mat1.racineCarree();
+
+    	double[][] val2 = {{1,2,3},{2,1,3},{5,4,3}};
+   	Fonction f2 = new Fonction(val2);
+
+ 	double[] tab1 = Fonction.get(mat1);
+ 	double[] tab2 = Fonction.get(f2);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
+
+    public void testNormeColonne(){
+	double[][] f1 = {{2,5,-7},{10,12,15}};
+    	MatR mat1 = new MatR(f1);
+	MatR mat1bis = mat1.normeColonnes();
+
+	double[][] val2 = {{2*2+10*10,5*5+12*12,7*7+15*15}};
+	MatR mat2 = new MatR(val2);
+	mat2.racineCarree();
+	double[][] val2bis = mat2.MatRToDouble();
+   	Fonction f2 = new Fonction(val2bis);
+
+ 	double[] tab1 = Fonction.get(mat1bis);
+ 	double[] tab2 = Fonction.get(f2);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
+
+public void testMoyenneColonne(){
+	double[][] f1 = {{2,5,-7},{10,12,15}};
+    	MatR mat1 = new MatR(f1);
+	MatR mat1bis = mat1.moyenneColonnes();
+
+	double[][] val2 = {{(mat1.get(1,1)+mat1.get(2,1))/2,(mat1.get(1,2)+mat1.get(2,2))/2,(mat1.get(1,3)+mat1.get(2,3))/2}};
+   	Fonction f2 = new Fonction(val2);
+
+ 	double[] tab1 = Fonction.get(mat1bis);
+ 	double[] tab2 = Fonction.get(f2);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
 
 }
