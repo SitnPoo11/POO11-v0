554e74a89828451ff5c145e3a00eae01d191af6e
siao <siao@euclid.univ-lyon1.fr>
2011-03-25 12:08:04 +0100

    Fin des tests avec diagonalisation d'une matrice

--- tests/TestMatR.java
+++ tests/TestMatR.java
@@ -179,14 +179,15 @@ public class TestMatR extends TestCase{
 	MatR mat2 = new MatR(val2);
 	mat2.racineCarree();
 	double[][] val2bis = mat2.MatRToDouble();
-   	Fonction f2 = new Fonction(val2bis);
+	double[][] val2ter = {{val2bis[1][1],val2bis[1][2],val2bis[1][3]}};
+   	Fonction f2 = new Fonction(val2ter);
 
  	double[] tab1 = Fonction.get(mat1bis);
  	double[] tab2 = Fonction.get(f2);
 	assertArrayEquals(tab1,tab2,0.1);
     }
 
-public void testMoyenneColonne(){
+    public void testMoyenneColonne(){
 	double[][] f1 = {{2,5,-7},{10,12,15}};
     	MatR mat1 = new MatR(f1);
 	MatR mat1bis = mat1.moyenneColonnes();
@@ -197,6 +198,90 @@ public void testMoyenneColonne(){
  	double[] tab1 = Fonction.get(mat1bis);
  	double[] tab2 = Fonction.get(f2);
 	assertArrayEquals(tab1,tab2,0.1);
+    }
+
+    public void testVarianceColonne(){
+	double[][] val1 = {{1,4,9},{4,1,9},{25,16,9}};
+    	MatR mat7 = new MatR(val1);
+	MatR mat14 = mat7.moyenneColonnes();
+
+	double[][] val2 = {{((mat7.get(1,1)-mat14.get(1,1))*(mat7.get(1,1)-mat14.get(1,1))+(mat7.get(2,1)-mat14.get(1,1))*(mat7.get(2,1)-mat14.get(1,1))+(mat7.get(3,1)-mat14.get(1,1))*(mat7.get(3,1)-mat14.get(1,1)))/3,((mat7.get(1,2)-mat14.get(1,2))*(mat7.get(1,2)-mat14.get(1,2))+(mat7.get(2,2)-mat14.get(1,2))*(mat7.get(2,2)-mat14.get(1,2))+(mat7.get(3,2)-mat14.get(1,2))*(mat7.get(3,2)-mat14.get(1,2)))/3,((mat7.get(1,3)-mat14.get(1,3))*(mat7.get(1,3)-mat14.get(1,3))+(mat7.get(2,3)-mat14.get(1,3))*(mat7.get(2,3)-mat14.get(1,3))+(mat7.get(3,3)-mat14.get(1,3))*(mat7.get(3,3)-mat14.get(1,3)))/3}};
+	Fonction f2 = new Fonction(val2);
+
+ 	double[] tab1 = Fonction.get(mat7.varianceColonnes());
+ 	double[] tab2 = Fonction.get(f2);
+	assertArrayEquals(tab1,tab2,0.1);
+
+    }
+
+    public void testCentrer(){
+	double[][] val1 = {{1,4,9},{4,1,9},{25,16,9}};
+    	MatR mat7 = new MatR(val1);
+	MatR mat14 = mat7.moyenneColonnes();
+
+	double[][] val2 = {{mat7.get(1,1)-mat14.get(1,1),mat7.get(1,2)-mat14.get(1,2),mat7.get(1,3)-mat14.get(1,3)},{mat7.get(2,1)-mat14.get(1,1),mat7.get(2,2)-mat14.get(1,2),mat7.get(2,3)-mat14.get(1,3)},{mat7.get(3,1)-mat14.get(1,1),mat7.get(3,2)-mat14.get(1,2),mat7.get(3,3)-mat14.get(1,3)}};
+	Fonction f2 = new Fonction(val2);
+
+	double[] tab1 = Fonction.get(mat7.centrer());
+ 	double[] tab2 = Fonction.get(f2);
+	assertArrayEquals(tab1,tab2,0.1);
+
+    }
+
+    public void testReduire(){
+	double[][] val1 = {{1,4,9},{4,1,9},{25,16,9}};
+    	MatR mat19 = new MatR(val1);
+	MatR mat16 = mat19.varianceColonnes();
+	mat16.racineCarree();
+
+	double[][] tab10 = {{mat19.get(1,1)/mat16.get(1,1),mat19.get(1,2)/mat16.get(1,2),mat19.get(1,3)/mat16.get(1,3)},{mat19.get(2,1)/mat16.get(1,1),mat19.get(2,2)/mat16.get(1,2),mat19.get(2,3)/mat16.get(1,3)},{mat19.get(3,1)/mat16.get(1,1),mat19.get(3,2)/mat16.get(1,2),mat19.get(3,3)/mat16.get(1,3)}};
+	Fonction f2 = new Fonction(tab10);
+
+	double[] tab1 = Fonction.get(mat19.reduire());
+ 	double[] tab2 = Fonction.get(f2);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
+
+    public void testProduitTranspose(){
+	MatR mat21 = new MatR(3,3,1);
+	mat21.set(1,1,15);  
+	mat21.set(1,3,-1);
+	mat21.set(2,1,5);
+	mat21.set(3,3,20);
+	double[][] tab11 = {{251,21,10},{21,3,20},{10,20,402}};
+
+	Fonction f2 = new Fonction(tab11);
+
+	double[] tab1 = Fonction.get(mat21.XtX());
+ 	double[] tab2 = Fonction.get(f2);
+	assertArrayEquals(tab1,tab2,0.1);
+    }
+
+
+    public void testDiag(){
+	double[][] tab12 = {{5,2,9,-6},{2,5,-6,9},{9,-6,5,2},{-6,9,2,5}};  
+    	MatR mat23 = new MatR(tab12);
+	MatR q = new MatR();
+	MatR lbd = new MatR();
+	mat23.diag(q,lbd);
+	mat23.afficheSimple("Matrice");
+	q.afficheSimple("Vecteurs propres");
+	lbd.afficheSimple("Valeurs propres");
+
+	double[][] tab13 = {{18},{10},{4},{-12}};   
+	Fonction f2 = new Fonction(tab13);
+
+	double[][] tab14 = {{0.5,0.5,-0.5,0.5},{-0.5,0.5,-0.5,-0.5},{0.5,0.5,0.5,-0.5},{-0.5,0.5,0.5,0.5}};   
+	Fonction f3 = new Fonction(tab14);
+
+
+	double[] tab1 = Fonction.get(lbd);
+ 	double[] tab2 = Fonction.get(f2);
+ 	double[] tab3 = Fonction.get(f3);
+ 	double[] tab4 = Fonction.get(q);
+	assertArrayEquals(tab1,tab2,0.1);
+	assertArrayEquals(tab4,tab3,0.1);
+
     }
 
 }
